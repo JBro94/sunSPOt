49c4a9008d1809586a6dc2f3de5c126ae2149df1
Jim B <jldbrokaw@gmail.com>
2022-04-10 14:22:03 -0500

    Initial base map and data for sunSPOt

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Map from './components/map'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>sunSPOt Mapp</p>
       </header>
+      <Map />
+      <footer className='App-footer'>
+        <p>Created By James Brokaw</p>
+      </footer>
     </div>
   );
 }
---
+++ src/components/map.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactMapGL, {Source, Layer} from 'react-map-gl'; 
+import 'mapbox-gl/dist/mapbox-gl.css';
+import bloNoLots from '../data/BloNoLots_PCS_FeaturesToJSON.geojson'
+
+//MapBox access token
+const MAPBOX_TOKEN = 
+"pk.eyJ1Ijoiamxicm9rIiwiYSI6ImNsMXB1b3dxdzAxdzUzY3M2MzEwMjMxcWkifQ.Jis7AzQxUBN6clwg2WuGIQ";
+
+const layerStyle = {
+    id: 'Lots',
+    type: 'fill',
+    paint: {
+        'fill-color': '#dd4545',
+        'fill-opacity': .5
+    }
+};
+
+//functional Component that loads the base map
+export default function Map(){
+
+
+    return (
+        <div>
+            <ReactMapGL 
+            initialViewState={{
+                latitude: 40.5,
+                longitude: -88.977,
+                zoom: 12
+            }}
+            style={{width: '100vw', height: '90vh'}}
+            mapStyle="mapbox://styles/mapbox/streets-v9"
+            mapboxAccessToken={MAPBOX_TOKEN}>
+                <Source id='parkingLots' type='geojson' data ={bloNoLots}>
+                <Layer {...layerStyle} />
+                </Source>
+            </ReactMapGL>
+        </div>
+    )
+}
