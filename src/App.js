5dc714d10dc5d8cf0809b715d30b15dded9df786
Jim B <jldbrokaw@gmail.com>
2022-04-19 19:50:53 -0500

    added modal to display information

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Map from './components/map';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from './components/card';
 import RouteCalculator from './components/routeCalculator';
+import {Typography, Modal, Box} from '@mui/material';
 import lots from './data/sunSPOtParking.json';
 
 function App() {
@@ -12,6 +13,9 @@ function App() {
   const [startLocale, setStartLocale] = useState(lots.features[1].geometry.coordinates);
   const [destLocale, setDestLocale] = useState(lots.features[0].geometry.coordinates);
   const [isRouteDisplayed, setIsRouteDisplay] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  const handleClose = () => setOpen(false);
 
   const handleStartChange = (e) => {
       setStartLocale(e.target.value);
@@ -22,7 +26,6 @@ function App() {
   };
 
   const handleRouteCalculation = async () => {
-          setIsRouteDisplay(true);
           const locales = `${startLocale[0]},${startLocale[1]};${destLocale[0]},${destLocale[1]}`;
           const query = await fetch(
             `https://api.mapbox.com/directions/v5/mapbox/driving/${locales}?geometries=geojson&access_token=pk.eyJ1Ijoiamxicm9rIiwiYSI6ImNsMXB1b3dxdzAxdzUzY3M2MzEwMjMxcWkifQ.Jis7AzQxUBN6clwg2WuGIQ`,
@@ -38,6 +41,8 @@ function App() {
                   coordinates: data.geometry.coordinates
               }
           };
+          setIsRouteDisplay(true);
+          setOpen(true);
           let distance = data.distance * .00062137;
           let chargeTime = (distance/25)*60;
           setRoute(
@@ -51,6 +56,18 @@ function App() {
           );
           console.log('completed')
   };
+
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  }
 
   useEffect(() => {
       console.log(route);
@@ -73,6 +90,23 @@ function App() {
           />
         </div>
       </header>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby = 'modal-modal-title'
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Route Information
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Distance: {route.distance} miles
+            Time Required at Charge Station: {route.chargeTime} min
+          </Typography>
+        </Box>
+
+      </Modal>
       <section>
         <Map
           displayRoute={isRouteDisplayed}
--- src/components/loaderIcon.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import "../App.css";
-
-export default function LoaderIcon(){
-    return (
-        <div className='loader'>
-        </div>
-    );
-}
